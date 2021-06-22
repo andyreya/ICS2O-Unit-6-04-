90011250bb2c2958099b100006e972a26ea7ef3d
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-22 01:31:17 +0000

    strings

--- script.js
+++ script.js
@@ -7,31 +7,33 @@ let area = 0
 document.getElementById('div').addEventListener('click', calculate)
 /* Once the function is activated, it will assign whatever number was typed into the input boxes to the variables and turn them into integers to work with. After that it will take the assigned values of those variables and run through the area of a trapezoid formula and assign the result to the 'area' variable. */
 function calculate () {
-  inputA = document.getElementById('inputA').value
+  inputA = document.getElementById('input-a').value
   inputA = parseInt(inputA)
-  inputB = document.getElementById('inputB').value
+  inputB = document.getElementById('input-b').value
   inputB = parseInt(inputB)
-  inputH = document.getElementById('inputH').value
+  inputH = document.getElementById('input-h').value
   inputH = parseInt(inputH)
   area = (inputA + inputB) / 2 * inputH
-  document.getElementById('inputA').disabled = false
-  document.getElementById('inputB').disabled = false
-  document.getElementById('inputH').disabled = false
+  document.getElementById('input-a').disabled = false
+  document.getElementById('input-b').disabled = false
+  document.getElementById('input-h').disabled = false
   document.getElementById('area-result').innerHTML = area
 }
-if (inputA == 0) {
-  document.getElementById("inputA").innerHTML = "Value cannot be 0"
+if (inputA === 0) {
+  document.getElementById('input-a').innerHTML = 'Value cannot be 0'
 }
 if (inputA < 0) {
-  document.getElementById("inputA").innerHTML = "Value cannot be a negative integer"
-}if (inputB == 0) {
-  document.getElementById("inputB").innerHTML = "Value cannot be 0"
+  document.getElementById('inputA-a').innerHTML = 'Value cannot be a negative integer'
+}
+if (inputB === 0) {
+  document.getElementById('input-b').innerHTML = 'Value cannot be 0'
 }
 if (inputB < 0) {
-  document.getElementById("inputB").innerHTML = "Value cannot be a negative integer"
-}if (inputH == 0) {
-  document.getElementById("inputH").innerHTML = "Value cannot be 0"
+  document.getElementById('input-b').innerHTML = 'Value cannot be a negative integer'
+}
+if (inputH === 0) {
+  document.getElementById('input-h').innerHTML = 'Value cannot be 0'
 }
 if (inputH < 0) {
-  document.getElementById("inputH").innerHTML = "Value cannot be a negative integer"
+  document.getElementById('input-h').innerHTML = 'Value cannot be a negative integer'
 }

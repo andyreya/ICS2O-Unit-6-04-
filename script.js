c5b0d638a06b93b338d9d62d4528010fde4d80fd
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-22 02:09:46 +0000

    variables

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ document.getElementById('div').addEventListener('click', calculate)
 /* Once the function is activated, it will assign whatever number was typed into the input boxes to the variables and turn them into integers to work with. After that it will take the assigned values of those variables and run through the area of a trapezoid formula and assign the result to the 'area' variable. */
 function calculate () {
   inputA = document.getElementById('input-a').value
-  inputA = parseInt(input-a)
+  inputA = parseInt(inputA)
   inputB = document.getElementById('input-b').value
-  inputB = parseInt(input-b)
+  inputB = parseInt(inputB)
   inputH = document.getElementById('input-h').value
-  inputH = parseInt(input-h)
+  inputH = parseInt(inputH)
   area = (inputA + inputB) / 2 * inputH
   document.getElementById('input-a').disabled = false
   document.getElementById('input-b').disabled = false
